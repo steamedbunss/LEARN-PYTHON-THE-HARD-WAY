e9f8c197c6f82bcea48792d824c17c4dd578979c
Terry Chen <terrych97@gmail.com>
2019-06-01 20:01:51 +0800

    completed 01.py

--- 01.py
+++ 01.py
@@ -1,1 +1,7 @@
-print("hello world")
+print("hello world!")
+print("hello Again")
+print("I like typing this")
+print("This is fun.")
+print("Yay! Printing")
+print("I'd prefer rather you 'not'.")
+print("I 'said' do not touch this.'")
